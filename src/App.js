47f1a500f7e3987448e559687266cdcd6a45aad6
Annie Huang <annie.hn@gmail.com>
2020-03-07 18:15:40 +1100

    Created the IncomeExpenses.js component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Balance} from "./components/Balance";
+import {IncomeExpenses} from "./components/IncomeExpenses";
 
 function App() {
     return (
@@ -9,7 +10,9 @@ function App() {
             <Header />
             <div className="container">
                 <Balance/>
+                <IncomeExpenses />
             </div>
+
         </>
     );
 }
