d19641a345e373511923209e722105f28bdca422
Annie Huang <annie.hn@gmail.com>
2020-03-07 21:28:19 +1100

    Make some adjustment on Transaction.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {GlobalContext} from "../context/GlobalState";
-import Transaction from "./Transaction";
+import {Transaction} from "./Transaction";
 
 export const TransactionList = () => {
     // const context = useContext(GlobalContext);
