8c3c7ecb3d8a500d1f6072266ff6a9d2820bff24
Annie Huang <annie.hn@gmail.com>
2020-03-21 21:05:39 +1100

    C:\react\expense-tracker-react\client>npm run build

---
+++ client/build/precache-manifest.595dbe4d3c58a001d4301b194eb74337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a77b09af913fbdb1b62e02104f3c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96fca36be370b52fb92",
+    "url": "/static/css/main.cec646b3.chunk.css"
+  },
+  {
+    "revision": "6e37e84ec2000c7afc17",
+    "url": "/static/js/2.e69574da.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e69574da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96fca36be370b52fb92",
+    "url": "/static/js/main.c2a53641.chunk.js"
+  },
+  {
+    "revision": "ec8a337eab2a1d4d6c72",
+    "url": "/static/js/runtime-main.b620963e.js"
+  }
+]);
