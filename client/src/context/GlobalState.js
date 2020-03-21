150a39ce7c67dffe976e9fd3157d48a47c478933
Annie Huang <annie.hn@gmail.com>
2020-03-21 18:43:26 +1100

    Add action for getTransactions

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,15 +1,21 @@
 import React, {createContext, useReducer} from "react";
 import AppReducer from "./AppReducer";
+import axios from 'axios';
 
 // Initial state
+// const initialState = {
+//     // transactions: [
+//     //     { id: 1, text: 'Flower', amount: -20 },
+//     //     { id: 2, text: 'Salary', amount: 300 },
+//     //     { id: 3, text: 'Book', amount: -10 },
+//     //     { id: 4, text: 'Camera', amount: 150 }
+//     // ]
+//     transactions: []
+// };
 const initialState = {
-    // transactions: [
-    //     { id: 1, text: 'Flower', amount: -20 },
-    //     { id: 2, text: 'Salary', amount: 300 },
-    //     { id: 3, text: 'Book', amount: -10 },
-    //     { id: 4, text: 'Camera', amount: 150 }
-    // ]
-    transactions: []
+    transactions: [],
+    error: null,
+    loading: true
 };
 
 // Create context
@@ -20,6 +26,25 @@ export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     // Actions
+    async function getTransactions() {
+        try {
+            // Because we have the proxy of http://localhost:5000 in client/package.json, you don't need to add it here.
+            const res = await axios.get('/api/v1/transactions');
+
+            dispatch({
+                type: 'GET_TRANSACTION',
+                payload: res.data.data
+            });
+
+        } catch (err) {
+            // The error in Note13 - Postman on POST with backend connected to mongoose with error output in resp.jpg
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            });
+        }
+    }
+
     function deleteTransaction(id) {
         dispatch({
             type: 'DELETE_TRANSACTION',
